f5d0e3b47be27594cdd49648fae9e251afced613
tamsekar <atamsekar@pulsesecure.net>
2022-03-13 00:25:40 -0800

    Corrections to categories in Expense.py

--- budget/Expense.py
+++ budget/Expense.py
@@ -43,8 +43,9 @@ class Expenses():
     
     def categorize_set_comprehension(self):
         necessary_expenses = {x for x in self.list 
-        if x.category == 'Auto and Gas' or x.category == 'Classes' or x.category == 'Utilities' or x.category == 'Mortgage'}
-        food_expenses = {x for x in self.list if (x.category == 'Groceries' or x.category == 'Eating')}
+                            if x.category == 'Phone' or x.category == 'Auto and Gas' or x.category == 'Classes' or x.category == 'Utilities' or x.category == 'Mortgage'}
+        food_expenses = {x for x in self.list 
+                       if x.category == 'Groceries' or x.category == 'Eating Out'}
         unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
         return necessary_expenses,food_expenses,unnecessary_expenses
     
